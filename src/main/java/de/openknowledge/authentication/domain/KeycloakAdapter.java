ac806c98607550a540139209fdb3577a693bb844
sonic <pieter.igel@ms.ingenico.com>
2020-07-28 02:49:33 +0200

    feat(keycloak-admin-client): Quarkus

--- src/main/java/de/openknowledge/authentication/domain/KeycloakAdapter.java
+++ src/main/java/de/openknowledge/authentication/domain/KeycloakAdapter.java
@@ -15,14 +15,17 @@
  */
 package de.openknowledge.authentication.domain;
 
+import java.net.MalformedURLException;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.client.ClientBuilder;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.admin.client.ClientBuilderWrapper;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.ClientResource;
@@ -49,7 +52,11 @@ public class KeycloakAdapter {
 
   @Inject
   public KeycloakAdapter(KeycloakAdapterConfiguration adapterConfig) {
-    ResteasyClient restClient = new ResteasyClientBuilder().connectionPoolSize(adapterConfig.getConnectionPoolSize()).build();
+
+    ResteasyClient c = (ResteasyClient) ResteasyClientBuilder.newClient();
+    ClientBuilder clientBuilder = ClientBuilderWrapper.create(c.getSslContext(), false);
+    ResteasyClient restClient = (ResteasyClient) clientBuilder.build();
+
     keycloak = KeycloakBuilder.builder()
         .serverUrl(adapterConfig.getServerUrl())
         .realm(adapterConfig.getMasterRealm())
@@ -57,7 +64,7 @@ public class KeycloakAdapter {
         .username(adapterConfig.getUsername())
         .password(adapterConfig.getPassword())
         .clientId(adapterConfig.getClientId())
-        .resteasyClient(restClient)
+        .resteasyClient(null)
         .build();
     tokenService = restClient.target(adapterConfig.getServerUrl()).proxy(TokenService.class);
   }
